284156a69f9236e7d81178cca7d0bf48583585a5
Darya Dmitrenok <dmitrenokdarya@gmail.com>
2024-10-26 19:52:00 +0300

    8kyu_reversedStrings

---
+++ 8kyu/reversedStrings.js
@@ -0,0 +1,12 @@
+//Complete the solution so that it reverses the string passed into it.
+
+
+//Solution
+
+function solution(str){
+    let newStr = '';
+    for (let i = str.length - 1; i >= 0; i--){
+        newStr += str[i];
+    }
+    return newStr;
+}
