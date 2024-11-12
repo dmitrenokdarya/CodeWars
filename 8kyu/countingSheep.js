7391a92d28efbce700ef774c4b62e235b9cce4e9
Darya Dmitrenok <dmitrenokdarya@gmail.com>
2024-11-12 22:38:02 +0300

    8kyu_countingSheep

---
+++ 8kyu/countingSheep.js
@@ -0,0 +1,12 @@
+//Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+//Hint: Don't forget to check for bad values like null/undefined
+
+
+//Solution
+
+function countSheeps(sheep) {
+    const countOfSheeps = sheep.filter((item) => {
+        return item === true
+    }) 
+    return countOfSheeps.length;
+};
